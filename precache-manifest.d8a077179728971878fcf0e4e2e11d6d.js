9bf3d2ffee77de422b0b6f49e6f7a699b4dc66f8
avakrishn <avakrishn@gmail.com>
2019-04-23 23:52:24 -0700

    Updates

---
+++ precache-manifest.d8a077179728971878fcf0e4e2e11d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "19184f5597a83f3c4479",
+    "url": "/tic-tac-toe/static/js/main.8d6686a3.chunk.js"
+  },
+  {
+    "revision": "c9669485980e70586603",
+    "url": "/tic-tac-toe/static/js/2.a33b33f6.chunk.js"
+  },
+  {
+    "revision": "19184f5597a83f3c4479",
+    "url": "/tic-tac-toe/static/css/main.bc37cdb5.chunk.css"
+  },
+  {
+    "revision": "838b9c33191c0ddcc6e0692965331361",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
